8fbc5d4c5eee86c01b0550486024329b8eb6889a
Abdi Ahmednur <92820358+AbdiAhmednur@users.noreply.github.com>
2023-04-11 00:41:23 +0100

    my changes

--- my-app/src/Component/Contact.js
+++ my-app/src/Component/Contact.js
@@ -6,7 +6,7 @@ function Contact(){
         <h1>Contact Me</h1> 
         <p>Want to work together?</p>
         <p><strong>Email:</strong> <a href="mailto:aabdi35@hotmail.com">aabdi35@hotmail.com</a> </p>
-        <p><strong>Linkedin:</strong> <a href="www.linkedin.com/in/abdi-ahmednur/">Abdi Ahmednur</a></p>
+        <p><strong>Linkedin:</strong> <a href="https://www.linkedin.com/in/abdi-ahmednur/" rel="noreferrer" > Abdi Ahmednur</a> </p>
       </section>
     
     )
--- my-app/src/Component/landingPage.js
+++ my-app/src/Component/landingPage.js
@@ -8,12 +8,13 @@ function landingPage() {
           Hi, my name is <strong className="name">Abdirahman Ahmednur</strong>. I am a Web Developer.
         </h1>
       </section>
+      <br />
       <div className="lb">
         <button> <a href="#about">About me</a></button>
         <button> <a href="#projects">Projects</a></button>
         <button> <a  href="#contact">Contact me</a></button>
         <button><a href={CV} target="_blank" download >My CV</a></button>
-        <button><a href="https://github.com/AbdiAhmednur">Github</a></button>
+        <button><a href="https://github.com/AbdiAhmednur" target='_blank' rel="noreferrer">Github</a></button>
       </div>
     </>
   );
